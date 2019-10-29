24debaff6d914834586518d19b000b8d87633f88
Timo Aho <zeukkari@gmail.com>
2019-10-29 08:30:26 +0200

    add Card component story

---
+++ src/common/card/Card.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Card from './Card';
+
+export default {
+  component: Card,
+  title: 'Card',
+};
+
+export const sampleCard = () => <Card title="heading">content</Card>;
