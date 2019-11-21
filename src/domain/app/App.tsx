0b9feb5403af8b2c1274ed2aa348db06bdc061ea
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-11-21 15:31:40 +0200

    Route for Individual Harbor page

--- src/domain/app/App.tsx
+++ src/domain/app/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HarborsPage from '../harbors/HarborsPageContainer';
+import IndividualHarborPage from '../individualHarbor/IndividualHarborPageContainer';
+
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={HarborsPage} />
+        <Route path="/harbors/:id" component={IndividualHarborPage} />
         <Route path="/harbors" component={HarborsPage} />
       </Switch>
     </Router>
