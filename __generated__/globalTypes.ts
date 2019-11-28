de5b42912752d6eab3d7993a9eb0c76cdbc9a9b0
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-11-28 16:22:20 +0200

    Remove /generated/ from .gitignore

---
+++ __generated__/globalTypes.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum BerthMooringType {
+  DINGHY_PLACE = "DINGHY_PLACE",
+  NO_STERN_TO_MOORING = "NO_STERN_TO_MOORING",
+  QUAYSIDE_MOORING = "QUAYSIDE_MOORING",
+  SEA_BUOY_MOORING = "SEA_BUOY_MOORING",
+  SIDE_SLIP_PLACE = "SIDE_SLIP_PLACE",
+  SINGLE_SLIP_PLACE = "SINGLE_SLIP_PLACE",
+  STERN_BUOY_PLACE = "STERN_BUOY_PLACE",
+  STERN_POLE_MOORING = "STERN_POLE_MOORING",
+  TRAWLER_PLACE = "TRAWLER_PLACE",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
