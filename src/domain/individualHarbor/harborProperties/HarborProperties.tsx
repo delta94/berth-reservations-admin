bed5d5aa382c83166fabf44d2a24a79444dfe5be
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-12-04 17:12:48 +0200

    Add translations

--- src/domain/individualHarbor/harborProperties/HarborProperties.tsx
+++ src/domain/individualHarbor/harborProperties/HarborProperties.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import styles from './harborProperties.module.scss';
 import Card from '../../../common/card/Card';
 import Text from '../../../common/text/Text';
 import Grid from '../../../common/grid/Grid';
-import Icon from '../../../common/icon/Icon';
 import ExternalLink from '../../../common/externalLink/ExternalLink';
 import Property from '../property/Property';
 
@@ -31,52 +31,58 @@ const HarborProperties: React.SFC<Props> = ({
   url,
   properties,
 }) => {
+  const { t } = useTranslation();
+
   return (
-    <Card title="SATAMA">
+    <Card title={t('individualHarbor.harborProperties.title')}>
       <Grid>
-        <img src={imageUrl} className={styles.image} />
+        <img alt="Harbor's location" src={imageUrl} className={styles.image} />
         <div>
           <Text as="h2" size="xl">
             {name}
           </Text>
           <ExternalLink to={url}>{address}</ExternalLink>
         </div>
-        <div>
-          <Grid colsCount={4}>
-            <Property
-              counter={properties.numberOfPlaces}
-              label="Paikkoja"
-              active={!!properties.numberOfPlaces}
-            />
-            <Property iconName="globe" label="Jonkin verran jonoa" />
-            <Property
-              iconName="plug"
-              label="Sähkö"
-              active={properties.electricity}
-            />
-            <Property
-              iconName="waterTap"
-              label="water"
-              active={properties.water}
-            />
-            <Property counter={properties.maximumWidth} label="Max. leveys" />
-            <Property
-              iconName="trash"
-              label="Jätehuolto"
-              active={properties.wasteCollection}
-            />
-            <Property
-              iconName="fence"
-              label="Portti"
-              active={properties.gate}
-            />
-            <Property
-              iconName="streetLight"
-              label="Valaistus"
-              active={properties.lighting}
-            />
-          </Grid>
-        </div>
+        <Grid colsCount={4}>
+          <Property
+            counter={properties.numberOfPlaces}
+            label={t('individualHarbor.harborProperties.numberOfPlaces')}
+            active={!!properties.numberOfPlaces}
+          />
+          <Property
+            iconName="globe"
+            label={t('individualHarbor.harborProperties.queue')}
+          />
+          <Property
+            iconName="plug"
+            label={t('individualHarbor.harborProperties.electricity')}
+            active={properties.electricity}
+          />
+          <Property
+            iconName="waterTap"
+            label={t('individualHarbor.harborProperties.water')}
+            active={properties.water}
+          />
+          <Property
+            counter={properties.maximumWidth}
+            label={t('individualHarbor.harborProperties.maximumWidth')}
+          />
+          <Property
+            iconName="trash"
+            label={t('individualHarbor.harborProperties.wasteCollection')}
+            active={properties.wasteCollection}
+          />
+          <Property
+            iconName="fence"
+            label={t('individualHarbor.harborProperties.gate')}
+            active={properties.gate}
+          />
+          <Property
+            iconName="streetLight"
+            label={t('individualHarbor.harborProperties.lighting')}
+            active={properties.lighting}
+          />
+        </Grid>
       </Grid>
     </Card>
   );
