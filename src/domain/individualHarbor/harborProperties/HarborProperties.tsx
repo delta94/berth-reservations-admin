5ada0cef102cfa4f6ff01213b9f0a386f7892f0f
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-12-05 14:06:00 +0200

    Styles fixes

--- src/domain/individualHarbor/harborProperties/HarborProperties.tsx
+++ src/domain/individualHarbor/harborProperties/HarborProperties.tsx
@@ -35,55 +35,63 @@ const HarborProperties: React.SFC<Props> = ({
 
   return (
     <Card title={t('individualHarbor.harborProperties.title')}>
-      <Grid>
-        <img alt="Harbor's location" src={imageUrl} className={styles.image} />
-        <div>
-          <Text as="h2" size="xl">
-            {name}
-          </Text>
-          <ExternalLink to={url}>{address}</ExternalLink>
+      <div className={styles.harborProperties}>
+        <div className={styles.details}>
+          <img
+            alt="Harbor's location"
+            src={imageUrl}
+            className={styles.image}
+          />
+          <div>
+            <Text as="h2" size="xl">
+              {name}
+            </Text>
+            <ExternalLink to={url}>{address}</ExternalLink>
+          </div>
         </div>
-        <Grid colsCount={4}>
-          <Property
-            counter={properties.numberOfPlaces}
-            label={t('individualHarbor.harborProperties.numberOfPlaces')}
-            active={!!properties.numberOfPlaces}
-          />
-          <Property
-            iconName="globe"
-            label={t('individualHarbor.harborProperties.queue')}
-          />
-          <Property
-            iconName="plug"
-            label={t('individualHarbor.harborProperties.electricity')}
-            active={properties.electricity}
-          />
-          <Property
-            iconName="waterTap"
-            label={t('individualHarbor.harborProperties.water')}
-            active={properties.water}
-          />
-          <Property
-            counter={properties.maximumWidth}
-            label={t('individualHarbor.harborProperties.maximumWidth')}
-          />
-          <Property
-            iconName="trash"
-            label={t('individualHarbor.harborProperties.wasteCollection')}
-            active={properties.wasteCollection}
-          />
-          <Property
-            iconName="fence"
-            label={t('individualHarbor.harborProperties.gate')}
-            active={properties.gate}
-          />
-          <Property
-            iconName="streetLight"
-            label={t('individualHarbor.harborProperties.lighting')}
-            active={properties.lighting}
-          />
-        </Grid>
-      </Grid>
+        <div className={styles.properties}>
+          <Grid colsCount={4}>
+            <Property
+              counter={properties.numberOfPlaces}
+              label={t('individualHarbor.harborProperties.numberOfPlaces')}
+              active={!!properties.numberOfPlaces}
+            />
+            <Property
+              iconName="globe"
+              label={t('individualHarbor.harborProperties.queue')}
+            />
+            <Property
+              iconName="plug"
+              label={t('individualHarbor.harborProperties.electricity')}
+              active={properties.electricity}
+            />
+            <Property
+              iconName="waterTap"
+              label={t('individualHarbor.harborProperties.water')}
+              active={properties.water}
+            />
+            <Property
+              counter={properties.maximumWidth}
+              label={t('individualHarbor.harborProperties.maximumWidth')}
+            />
+            <Property
+              iconName="trash"
+              label={t('individualHarbor.harborProperties.wasteCollection')}
+              active={properties.wasteCollection}
+            />
+            <Property
+              iconName="fence"
+              label={t('individualHarbor.harborProperties.gate')}
+              active={properties.gate}
+            />
+            <Property
+              iconName="streetLight"
+              label={t('individualHarbor.harborProperties.lighting')}
+              active={properties.lighting}
+            />
+          </Grid>
+        </div>
+      </div>
     </Card>
   );
 };
