ef8c8e59b36f7bb25cc00ba1be30ed150792d77e
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-12-13 18:54:39 +0200

    Remove redundant functinos inherited from Kukkuu

--- src/domain/auth/authenticate.ts
+++ src/domain/auth/authenticate.ts
@@ -1,14 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
-
 import userManager from './userManager';
-import { StoreThunk } from '../app/types/AppTypes';
-import {
-  startFetchingToken,
-  fetchTokenSuccess,
-  fetchTokenError,
-} from './state/BackendAuthenticationActions';
-import { TUNNISTAMO_API_TOKEN_ENDPOINT } from '../api/constants/ApiConstants';
-import { BackendTokenResponse } from './types/BackendAuthenticationTypes';
 
 export const loginTunnistamo = (path?: string) => {
   userManager.signinRedirect(
@@ -18,27 +8,4 @@ export const loginTunnistamo = (path?: string) => {
 
 export const logoutTunnistamo = (path?: string) => {
   userManager.signoutRedirect(path ? { data: { path: path } } : {});
-};
-
-export const authenticateWithBackend = (
-  accessToken: string
-): StoreThunk => async dispatch => {
-  try {
-    dispatch(startFetchingToken());
-
-    const res: AxiosResponse<BackendTokenResponse> = await axios.post(
-      TUNNISTAMO_API_TOKEN_ENDPOINT,
-      {},
-      {
-        headers: {
-          Authorization: `bearer ${accessToken}`,
-        },
-      }
-    );
-
-    dispatch(fetchTokenSuccess(res.data));
-  } catch (error) {
-    dispatch(fetchTokenError(error));
-    // TODO: add error-handler
-  }
 };
