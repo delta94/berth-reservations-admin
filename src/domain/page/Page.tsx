dc91cefb548bf1a852b519f664451e0fd2df8c87
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-12-18 11:16:52 +0200

    Remove unused variable

--- src/domain/page/Page.tsx
+++ src/domain/page/Page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import Layout from '../../common/layout/Layout';
