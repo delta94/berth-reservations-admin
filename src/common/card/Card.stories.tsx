23ecad4bbc02cc6828e1f55934f7bacc604253cc
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-01-28 16:01:33 +0200

    Refactor Card component

--- src/common/card/Card.stories.tsx
+++ src/common/card/Card.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Card from './Card';
+import CardHeader from '../cardHeader/CardHeader';
+import CardBody from '../cardBody/CardBody';
 import Text from '../text/Text';
 import Section from '../section/Section';
 import LabelValuePair from '../labelValuePair/LabelValuePair';
@@ -10,56 +12,67 @@ export default {
   title: 'Card',
 };
 
-export const sampleCard = () => <Card title="heading">content</Card>;
+export const sampleCard = () => (
+  <Card>
+    <CardHeader title="heading" />
+    <CardBody>content</CardBody>
+  </Card>
+);
 
 export const Harbor1 = () => (
   <Card>
-    <Section title="Osoite">
-      <Text color="brand" size="xs">
-        Aurinkoranta 1, 00990 Helsinki
-      </Text>
-    </Section>
-    <Section>
-      <Text color="brand" size="s">
-        Toimipisteen nettisivut
-      </Text>
-    </Section>
-    <Section>
-      <Text color="brand" size="s">
-        Satamakartta (PDF)
-      </Text>
-    </Section>
-    <Section>
-      <Text color="brand" size="s">
-        Google maps
-      </Text>
-    </Section>
+    <CardBody>
+      <Section title="Osoite">
+        <Text color="brand" size="xs">
+          Aurinkoranta 1, 00990 Helsinki
+        </Text>
+      </Section>
+      <Section>
+        <Text color="brand" size="s">
+          Toimipisteen nettisivut
+        </Text>
+      </Section>
+      <Section>
+        <Text color="brand" size="s">
+          Satamakartta (PDF)
+        </Text>
+      </Section>
+      <Section>
+        <Text color="brand" size="s">
+          Google maps
+        </Text>
+      </Section>
+    </CardBody>
   </Card>
 );
 
 export const Harbor2 = () => (
   <Card>
-    <Section>
-      <LabelValuePair label="Max leveys" value="2.5m - 4m" />
-      <LabelValuePair
-        label="Kiinnitys"
-        value="Aisa-, Kävelyaisa- ja Peräpoijupaikkoja"
-      />
-      <LabelValuePair
-        label="Päällikkö"
-        value="Mikko Mallikas +358 00 000 000"
-      />
-      <LabelValuePair label="Huoltotiimi" value="Itäinen veneilytiimi" />
-    </Section>
+    <CardBody>
+      <Section>
+        <LabelValuePair label="Max leveys" value="2.5m - 4m" />
+        <LabelValuePair
+          label="Kiinnitys"
+          value="Aisa-, Kävelyaisa- ja Peräpoijupaikkoja"
+        />
+        <LabelValuePair
+          label="Päällikkö"
+          value="Mikko Mallikas +358 00 000 000"
+        />
+        <LabelValuePair label="Huoltotiimi" value="Itäinen veneilytiimi" />
+      </Section>
+    </CardBody>
   </Card>
 );
 
 export const Harbor3 = () => (
   <Card>
-    <Section title="Viimeaikainen toiminta">
-      <Text color="brand" size="xs">
-        Ei mitään
-      </Text>
-    </Section>
+    <CardBody>
+      <Section title="Viimeaikainen toiminta">
+        <Text color="brand" size="xs">
+          Ei mitään
+        </Text>
+      </Section>
+    </CardBody>
   </Card>
 );
--- src/common/card/Card.tsx
+++ src/common/card/Card.tsx
@@ -4,16 +4,12 @@ import classNames from 'classnames';
 import styles from './card.module.scss';
 
 export interface CardProps {
-  title?: string;
   children: React.ReactNode;
   className?: string;
 }
 
-const Card = ({ title, children, className }: CardProps) => (
-  <div className={classNames(styles.container, className)}>
-    {title && <header className={styles.header}>{title}</header>}
-    <section className={styles.body}>{children}</section>
-  </div>
+const Card: React.SFC<CardProps> = ({ children, className }) => (
+  <div className={classNames(styles.card, className)}>{children} </div>
 );
 
 export default Card;
