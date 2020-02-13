05b4cb6f3bfe45ef7e751fbffbbb25ec3e447cf6
Timo Aho <zeukkari@gmail.com>
2020-02-13 14:19:43 +0200

    abstract integration test

--- src/common/chip/Chip.stories.tsx
+++ src/common/chip/Chip.stories.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
+import { storiesOf } from "@storybook/react";
 
 import Chip from './Chip';
 
-export default {
-  component: Chip,
-  title: 'Chip',
-};
+storiesOf("Oldlabels", module)
+  .addParameters({
+    abstract: {
+      // Copy a collection or layer share url from Abstract
+      url: "https://share.goabstract.com/900afdee-766a-4c39-b8d3-abd23a53c9eb?"
+    }
+  })
+  // Name your stories after layers in the collection
+  .add("atoms/text/labe&tag/warning", () => <Chip label="Lorem ipsum" color="yellow" />)
+  .add("atoms/text/labe&tag/disabled", () => <Chip label="Lorem ipsum" color="grey" />)
+  .add("atoms/text/labe&tag/success", () => <Chip label="Lorem ipsum" color="green" />)
+  .add("atoms/text/labe&tag/info", () => <Chip label="Lorem ipsum" color="blue" />)
+  .add("atoms/text/labe&tag/fail", () => <Chip label="Lorem ipsum" color="red" />)
 
-export const chip = () => (
-  <>
-    <Chip label="Red" color="red" />
-    <Chip label="Orange" color="orange" />
-    <Chip label="Yellow" color="yellow" />
-    <Chip label="Green" color="green" />
-    <Chip label="Grey" color="grey" />
-    <Chip label="Blue" color="blue" />
-  </>
-);
+  // https://share.goabstract.com/900afdee-766a-4c39-b8d3-abd23a53c9eb?collectionLayerId=2cc256ed-2d9d-4cf7-b32d-dcf7e527c4fe&mode=design
 
-chip.story = {
-  name: 'Default',
-};
+// Name your stories after layers in the collection
+// export const blogIndex = () => <Harborsandberths {...dummyProps} />;
+// export const tagWarning = () => <Chip label="Foo" color="blue" />;
+// export const tagDisabled = () => <Chip label="Foo" color="blue" />;
+// export const tagSuccess = () => <Chip label="Foo" color="blue" />;
+// export const tagInfo = () => <Chip label="Foo" color="blue" />;
+
