97594b41b37b01cb4052a8e044062e895578e693
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-03-26 17:12:07 +0200

    Remove redundant libraries and modules

--- src/domain/auth/state/AuthenticationReducers.ts
+++
@@ -1,9 +0,0 @@
-import { reducer as oidcReducer } from 'redux-oidc';
-import { combineReducers } from 'redux';
-
-import backendAuthenticationReducer from './BackendAuthenticationReducer';
-
-export default combineReducers({
-  backend: backendAuthenticationReducer,
-  tunnistamo: oidcReducer,
-});
