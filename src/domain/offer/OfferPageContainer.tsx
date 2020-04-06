89aefb2617ec471492e92e39fcbd7ff02b16b887
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-04-06 17:28:06 +0300

    Define columns' widths of the offer page's table

--- src/domain/offer/OfferPageContainer.tsx
+++ src/domain/offer/OfferPageContainer.tsx
@@ -6,7 +6,7 @@ import { Notification } from 'hds-react';
 
 import LoadingSpinner from '../../common/spinner/LoadingSpinner';
 import { OFFER_PAGE_QUERY } from './queries';
-import Table, { Column } from '../../common/table/Table';
+import Table, { Column, COLUMN_WIDTH } from '../../common/table/Table';
 import OfferPage from './OfferPage';
 import InternalLink from '../../common/internalLink/InternalLink';
 import { OFFER_PAGE } from './__generated__/OFFER_PAGE';
@@ -53,35 +53,42 @@ const OfferPageContainer: React.FC = () => {
       ),
       Header: t('offer.tableHeaders.harbor') || '',
       accessor: 'harbor',
+      width: COLUMN_WIDTH.XL,
     },
     {
       Header: t('offer.tableHeaders.pier') || '',
       accessor: 'pier',
       filter: 'exactText',
+      width: COLUMN_WIDTH.S,
     },
     {
       Header: t('offer.tableHeaders.berth') || '',
       accessor: 'berth',
+      width: COLUMN_WIDTH.XS,
     },
     {
       Cell: ({ cell }) => formatDimension(cell.value, i18n.language),
       Header: t('offer.tableHeaders.width') || '',
       accessor: 'width',
+      width: COLUMN_WIDTH.XS,
     },
     {
       Cell: ({ cell }) => formatDimension(cell.value, i18n.language),
       Header: t('offer.tableHeaders.length') || '',
       accessor: 'length',
+      width: COLUMN_WIDTH.XS,
     },
     {
       Cell: ({ cell }) => formatDimension(cell.value, i18n.language),
       Header: t('offer.tableHeaders.draught') || '',
       accessor: 'draught',
+      width: COLUMN_WIDTH.XS,
     },
     {
       Cell: ({ cell }) => t([`common.mooringTypes.${cell.value}`, cell.value]),
       Header: t('offer.tableHeaders.mooringType') || '',
       accessor: 'mooringType',
+      width: COLUMN_WIDTH.S,
     },
   ];
 
