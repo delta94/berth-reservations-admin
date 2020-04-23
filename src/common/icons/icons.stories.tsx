10531b60afee8534ecf8adbd3aebfbb8df1ea300
Tatu Arvela <tatu.arvela@nitor.com>
2020-04-23 14:25:21 +0300

    Fix Icon story and add knobs to control it

--- src/common/icons/icons.stories.tsx
+++ src/common/icons/icons.stories.tsx
@@ -2,6 +2,10 @@ import path from 'path';
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { boolean, select, withKnobs } from '@storybook/addon-knobs';
+
+import Icon, { IconShapes } from './Icon';
+import * as Icons from './index';
 
 const Wrapper = ({ children, size }) => (
   <div
@@ -17,14 +21,41 @@ const Wrapper = ({ children, size }) => (
   </div>
 );
 
-const stories = storiesOf('Icons', module);
-
 const makeSvgStyleRules = (color: string) => ({
   fill: color,
   padding: '10px',
 });
 
-const req = require.context('.', false, /^.\/Icon.*.tsx$/);
+const stories = storiesOf('Icons', module);
+stories.addDecorator(withKnobs);
+stories.addDecorator(storyFn => (
+  <>
+    <div style={makeSvgStyleRules('#333')}>{storyFn()}</div>
+    <div style={{ background: '#333', ...makeSvgStyleRules('#fff') }}>
+      {storyFn()}
+    </div>
+  </>
+));
+
+const shapes: () => IconShapes[] = () => Object.keys(Icons) as IconShapes[];
+
+stories.add('Icon', () => (
+  <Wrapper size="200px">
+    <Icon
+      outlined={boolean('Outlined', false, 'Icon')}
+      shape={select('Shape', shapes(), 'IconAccessibility', 'Icon')}
+      size={select('Size', ['small', 'medium', 'large'], 'small', 'Icon')}
+      color={select(
+        'Color',
+        ['standard', 'disabled', 'brand', 'secondary'],
+        'standard',
+        'Icon'
+      )}
+    />
+  </Wrapper>
+));
+
+const req = require.context('.', false, /^.\/Icon.+.tsx$/);
 req.keys().forEach(fileName => {
   const Component = req(fileName).default;
   const componentName = path.basename(fileName, '.tsx');
@@ -32,34 +63,10 @@ req.keys().forEach(fileName => {
 
   stories.add(componentName, () => (
     <>
-      <div style={makeSvgStyleRules('#333')}>
-        <Wrapper size="200px">
-          <Component />
-        </Wrapper>
-        <Wrapper size="100px">
-          <Component />
-        </Wrapper>
-        <Wrapper size="50px">
-          <Component />
-        </Wrapper>
-        <Wrapper size="25px">
-          <Component />
-        </Wrapper>
-      </div>
-      <div style={{ background: '#333', ...makeSvgStyleRules('#fff') }}>
-        <Wrapper size="200px">
-          <Component />
-        </Wrapper>
-        <Wrapper size="100px">
-          <Component />
-        </Wrapper>
-        <Wrapper size="50px">
-          <Component />
-        </Wrapper>
-        <Wrapper size="25px">
-          <Component />
-        </Wrapper>
-      </div>
+      <Wrapper size="200px">{<Component />}</Wrapper>
+      <Wrapper size="100px">{<Component />}</Wrapper>
+      <Wrapper size="50px">{<Component />}</Wrapper>
+      <Wrapper size="25px">{<Component />}</Wrapper>
     </>
   ));
 });
