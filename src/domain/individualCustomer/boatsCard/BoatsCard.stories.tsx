3e5125f72278d6014c22c8ac3c22d651e17374c3
Santtu Laukkanen <santtu.laukkanen@gmail.com>
2020-05-04 13:14:22 +0300

    Fix BoatsCard storybook data

--- src/domain/individualCustomer/boatsCard/BoatsCard.stories.tsx
+++ src/domain/individualCustomer/boatsCard/BoatsCard.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import BoatsCard from './BoatsCard';
+import { Boat, LargeBoat } from '../types';
 
 export default {
   component: BoatsCard,
@@ -10,85 +11,43 @@ export default {
   ],
 };
 
-export const oneBoat = () => (
-  <BoatsCard
-    boats={[
-      {
-        id: '1234',
-        boatType: 'Purjevene / moottoripursi',
-        registrationNumber: 'A 12345',
-        boatWidth: '3,2 m',
-        boatLength: '6 m',
-        boatDepth: '0,8 m',
-        boatWeight: '350 kg',
-        boatName: 'Cama la Yano',
-        boatBrand: 'Marine',
-      },
-    ]}
-  />
-);
+const boat: Boat = {
+  id: '1111',
+  boatType: {
+    id: '7',
+    name: 'Troolari',
+  },
+  width: 2,
+  length: 10,
+  draught: 2,
+  weight: 15000,
+  name: 'Huh hah hei',
+  model: 'Trawl 11',
+  registrationNumber: 'TRLR-555',
+};
 
-export const multipleBoats = () => (
-  <BoatsCard
-    boats={[
-      {
-        id: '5432',
-        boatType: 'Purjevene / moottoripursi',
-        registrationNumber: 'A 67890',
-        boatWidth: '2,3 m',
-        boatLength: '4 m',
-        boatDepth: '0,8 m',
-        boatWeight: '200 kg',
-        boatName: 'Huh hah hei',
-        boatBrand: 'Boaty',
-      },
-      {
-        id: '5432',
-        boatType: 'Purjevene / moottoripursi',
-        registrationNumber: 'A 67890',
-        boatWidth: '2,3 m',
-        boatLength: '4 m',
-        boatDepth: '0,8 m',
-        boatWeight: '200 kg',
-        boatName: 'Huh hah hei',
-        boatBrand: 'Boaty',
-      },
-    ]}
-  />
-);
+const largeBoat: LargeBoat = {
+  id: '9999',
+  boatType: {
+    id: '8',
+    name: 'Suuri alus (yli 20t)',
+  },
+  width: 4,
+  length: 15,
+  draught: 2,
+  weight: 15000,
+  name: 'DevilsDriver',
+  model: 'Trawl 77',
+  registrationNumber: 'TRLR-666',
+  propulsion: 'Bensin',
+  hullMaterial: 'Aluminium',
+  intendedUse: 'For fun',
+  boatIsInspected: true,
+  boatIsInsured: true,
+};
 
-export const withLargeBoat = () => (
-  <BoatsCard
-    boats={[
-      {
-        id: '5432',
-        boatType: 'Purjevene / moottoripursi',
-        registrationNumber: 'A 67890',
-        boatWidth: '2,3 m',
-        boatLength: '4 m',
-        boatDepth: '0,8 m',
-        boatWeight: '200 kg',
-        boatName: 'Huh hah hei',
-        boatBrand: 'Boaty',
-      },
-      {
-        id: '9999',
-        boatType: 'Suuri alus (yli 20t)',
-        registrationNumber: 'A 67890',
-        boatWidth: '5,5 m',
-        boatLength: '22 m',
-        boatDepth: '1,5 m',
-        boatWeight: '24 000 kg',
-        boatName: 'Caraboudjan',
-        boatBrand: 'Buster',
-        boatPower: 'Polttoöljy',
-        boatMaterial: 'Teräs',
-        purpose:
-          'Kalastus kesällä, muuten huviveneily ja joskus viedään anoppi katsomaan valaita',
-        inspection:
-          'Voimassa 31.12.2019 Tarkastettu 24.12.2018 Santtu Satamapäällikkö',
-        insurance: 'Tarkastettu 24.12.2018 Santtu Satamapäällikkö',
-      },
-    ]}
-  />
-);
+export const oneBoat = () => <BoatsCard boats={[boat]} />;
+
+export const multipleBoats = () => <BoatsCard boats={[boat, boat]} />;
+
+export const withLargeBoat = () => <BoatsCard boats={[boat, largeBoat]} />;
