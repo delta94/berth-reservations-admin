d58698d039ca6d4ddb64d844ead6a72bfe3cc517
Aleksi Hietanen <aleksi@identio.fi>
2020-05-04 16:58:59 +0300

    Update individualHarbor utils test snapshot and mock data

--- src/domain/individualHarbor/forms/berthForm/BerthForm.tsx
+++ src/domain/individualHarbor/forms/berthForm/BerthForm.tsx
@@ -70,7 +70,9 @@ const BerthForm: React.FC<BerthFormProps> = ({
     length: Yup.number()
       .typeError(t('forms.common.errors.numberType'))
       .required(t('forms.common.errors.required')),
-    depth: Yup.mixed<number | null>().typeError(t('forms.common.errors.numberType')),
+    depth: Yup.mixed<number | null>().typeError(
+      t('forms.common.errors.numberType')
+    ),
     mooringType: Yup.string().required(t('forms.common.errors.required')),
   });
 
