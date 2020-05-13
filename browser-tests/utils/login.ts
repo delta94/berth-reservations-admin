f1cd62b3c782ae1c7b731c1e3c915888fbdcdbd6
Santtu Laukkanen <santtu.laukkanen@gmail.com>
2020-05-13 09:45:33 +0300

    Update login (no need for waits/reload anymore)

--- browser-tests/utils/login.ts
+++ browser-tests/utils/login.ts
@@ -1,7 +1,7 @@
-import { ssoLogin } from "../pages/ssoLogin";
-import { login as loginPage } from "../pages/login";
-import { testUsername, testUserPassword } from "./settings";
-import { navigation } from "../pages/navigation";
+import { ssoLogin } from '../pages/ssoLogin';
+import { login as loginPage } from '../pages/login';
+import { testUsername, testUserPassword } from './settings';
+import { navigation } from '../pages/navigation';
 
 export const login = async (t: TestController) => {
   await t
@@ -10,13 +10,6 @@ export const login = async (t: TestController) => {
     .typeText(ssoLogin.username, testUsername())
     .typeText(ssoLogin.password, testUserPassword())
     .click(ssoLogin.loginButton)
-    // For some reason the login page is displayed again even
-    // tough the user should be already logged in by sso
-    .expect(loginPage.loginButton.exists).ok();
-
-  // For some reason this reload is needed in order to display the app
-  // clicking the login button again doesn't work
-  await t.wait(2000);
-  await t.eval(() => location.reload());
-  await t.expect(navigation.sidebarContainer.exists).ok();
+    .expect(navigation.sidebarContainer.exists)
+    .ok();
 };
