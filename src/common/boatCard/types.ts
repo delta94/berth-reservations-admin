1e1202f0cbd9ae38a08b6832f77c44fce5bfb7ae
Aleksi Hietanen <aleksi@identio.fi>
2020-05-29 13:32:51 +0300

    Add applicantion's boat information to OfferPage

---
+++ src/common/boatCard/types.ts
@@ -0,0 +1,10 @@
+export type Boat = {
+  boatType: string | null;
+  boatRegistrationNumber: string | null;
+  boatName: string | null;
+  boatModel: string | null;
+  boatWidth: number | null;
+  boatLength: number | null;
+  boatDraught: number | null;
+  boatWeight: number | null;
+};
