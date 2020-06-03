832357fd6abb17de57466f6afd5eca4a69044b4d
Aleksi Hietanen <aleksi@identio.fi>
2020-06-03 09:49:25 +0300

    Change layout of ErrorPage

--- src/features/errorPage/ErrorPage.tsx
+++ src/features/errorPage/ErrorPage.tsx
@@ -5,23 +5,34 @@ import styles from './errorPage.module.scss';
 import Text from '../../common/text/Text';
 import Icon from '../../common/icons/Icon';
 import ExternalLink from '../../common/externalLink/ExternalLink';
+import Layout from '../../common/layout/Layout';
+import Header from '../../common/header/Header';
+import HelsinkiLogo from '../../common/helsinkiLogo/HelsinkiLogo';
 
 const ErrorPage: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div className={styles.container}>
-      <div className={styles.innerContainer}>
-        <Icon shape="IconExclamationCircle" size="large" />
-        <Text as="h1" className={styles.heading}>
-          {t('errorPage.heading')}
-        </Text>
-        <Text className={styles.text}>{t('errorPage.text')}</Text>
-        <ExternalLink href="/" target="_self">
-          {t('errorPage.linkText')}
-        </ExternalLink>
+    <Layout
+      header={
+        <Header>
+          <HelsinkiLogo size="large" color="white" />
+        </Header>
+      }
+    >
+      <div className={styles.container}>
+        <div className={styles.innerContainer}>
+          <Icon shape="IconExclamationCircle" size="large" />
+          <Text as="h1" className={styles.heading}>
+            {t('errorPage.heading')}
+          </Text>
+          <Text className={styles.text}>{t('errorPage.text')}</Text>
+          <ExternalLink href="/" target="_self">
+            {t('errorPage.linkText')}
+          </ExternalLink>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
