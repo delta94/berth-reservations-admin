0cec54f44d87075b5cf7efc8fba677c602f1db51
Aleksi Hietanen <aleksi@identio.fi>
2020-06-05 10:49:43 +0300

    Finish implementing HDSToastContainer tests

--- src/common/toast/__tests__/HDSToastContainer.test.tsx
+++ src/common/toast/__tests__/HDSToastContainer.test.tsx
@@ -15,7 +15,7 @@ describe('HDSToastContainer', () => {
         <button
           id="toastBtn"
           onClick={() => {
-            hdsToast({ type: 'error', labelText: 'labelText', text: 'text', toastId: count.toString() });
+            hdsToast({ type: 'error', labelText: 'labelText', text: 'text', toastId: `toast-${count}` });
             setCount(count + 1);
           }}
         />
@@ -29,31 +29,38 @@ describe('HDSToastContainer', () => {
 
   const simulateToast = async (wrapper: ReactWrapper) => {
     wrapper.find('#toastBtn').simulate('click');
-    await act(async () => await wait(10));
+    await act(async () => await wait(100));
+    wrapper.update();
+  };
+
+  const simulateCloseToast = async (wrapper: ReactWrapper, toastId: number) => {
+    wrapper.find(`#toast-${toastId}`).find('button[title="common.closeToast"]').simulate('click');
+    await act(async () => await wait(1500));
+    wrapper.update();
   };
 
   it('adds toasts to the container when hdsToast is called', async () => {
     const wrapper = getWrapper();
-    // TODO: assert number of toasts == 0
     await simulateToast(wrapper);
-    // TODO: Assert number of toasts == 1
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find('#toast-0')).toHaveLength(1);
+    expect(wrapper.find('.Toastify__toast')).toHaveLength(1);
   });
 
   it('supports multiple simultaneous toasts', async () => {
     const wrapper = getWrapper();
     await simulateToast(wrapper);
     await simulateToast(wrapper);
-    // TODO: assert number of toasts == 2
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find('#toast-0')).toHaveLength(1);
+    expect(wrapper.find('#toast-1')).toHaveLength(1);
+    expect(wrapper.find('.Toastify__toast')).toHaveLength(2);
   });
 
   it('closes toast when the notifications close button is clicked', async () => {
     const wrapper = getWrapper();
     await simulateToast(wrapper);
-    // TODO: assert number of toasts == 1
-    // TODO: click to close first toast and await
-    // TODO: assert number of toasts == 0
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find('#toast-0')).toHaveLength(1);
+    await simulateCloseToast(wrapper, 0);
+    expect(wrapper.find('#toast-0')).toHaveLength(0);
+    expect(wrapper.find('.Toastify__toast')).toHaveLength(0);
   });
 });
