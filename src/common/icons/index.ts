eb87f0d7185fa307ffe584dc4298e8322b14167a
Tatu Arvela <tatu.arvela@nitor.com>
2020-06-08 19:26:30 +0300

    Remove remaining HDS duplicate icons

--- src/common/icons/index.ts
+++ src/common/icons/index.ts
@@ -1,27 +1,22 @@
 export type { IconProps } from './Icon.interface';
 
 export { default as IconAccessibility } from './berthReservations/IconAccessibility';
-export { default as IconAngleRight } from './berthReservations/IconAngleRight';
 export { default as IconApplications } from './berthReservations/IconApplications';
-export { default as IconArrowLeft } from './berthReservations/IconArrowLeft';
-export { default as IconArrowRight } from './berthReservations/IconArrowRight';
 export { default as IconBoat } from './berthReservations/IconBoat';
 export { default as IconBusiness } from './berthReservations/IconBusiness';
 export { default as IconContracts } from './berthReservations/IconContracts';
+export { default as IconCustomers } from './berthReservations/IconCustomers';
 export { default as IconDirections } from './berthReservations/IconDirections';
 export { default as IconDivided } from './berthReservations/IconDivided';
 export { default as IconDollyEmpty } from './berthReservations/IconDollyEmpty';
 export { default as IconFence } from './berthReservations/IconFence';
 export { default as IconInvoice } from './berthReservations/IconInvoice';
 export { default as IconNoBoat } from './berthReservations/IconNoBoat';
-export { default as IconPerson } from './berthReservations/IconPerson';
 export { default as IconPlug } from './berthReservations/IconPlug';
 export { default as IconPole } from './berthReservations/IconPole';
 export { default as IconRead } from './berthReservations/IconRead';
 export { default as IconRegisteredBoat } from './berthReservations/IconRegisteredBoat';
 export { default as IconStreetLight } from './berthReservations/IconStreetLight';
-export { default as IconTimes } from './berthReservations/IconTimes';
-export { default as IconTrash } from './berthReservations/IconTrash';
 export { default as IconTrestle } from './berthReservations/IconTrestle';
 export { default as IconUnregisteredBoat } from './berthReservations/IconUnregisteredBoat';
 export { default as IconWaterTap } from './berthReservations/IconWaterTap';
