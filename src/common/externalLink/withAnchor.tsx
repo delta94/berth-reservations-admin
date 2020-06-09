e47408f44fb6ac2eb5f887042af5e95b5bd0e940
Tatu Arvela <tatu.arvela@nitor.com>
2020-06-09 10:30:34 +0300

    Adjust ExternalLink arrow icon sizes

--- src/common/externalLink/withAnchor.tsx
+++ src/common/externalLink/withAnchor.tsx
@@ -3,7 +3,6 @@ import classNames from 'classnames';
 import { IconAngleRight } from 'hds-react';
 
 import styles from './externalLink.module.scss';
-import IconWrapper from '../iconWrapper/IconWrapper';
 
 export type WithAnchorProps = {
   variant?: 'default' | 'withArrow';
@@ -17,9 +16,7 @@ const withAnchor = <P extends object>(Component: React.ComponentType<P>) => {
     return (
       <a className={classNames(styles.link, styles[variant])} href={href} target={target} rel="noopener noreferrer">
         {children}
-        {variant === 'withArrow' && (
-          <IconWrapper icon={IconAngleRight} color="brand" size="s" className={styles.arrow} />
-        )}
+        {variant === 'withArrow' && <IconAngleRight color="brand" size="m" className={styles.arrow} />}
       </a>
     );
   };
