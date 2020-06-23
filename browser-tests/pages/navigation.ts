e49c53716c32b690e14a99d595dbd2bdecc87665
Tatu Arvela <tatu.arvela@nitor.com>
2020-06-23 16:42:03 +0300

    Add rudimentary browser test for winterStorageAreas

--- browser-tests/pages/navigation.ts
+++ browser-tests/pages/navigation.ts
@@ -3,6 +3,7 @@ import { Selector } from 'testcafe';
 export const navigation = {
   sidebarContainer: Selector('div[class^="sidebar_mainWrapper"]'),
   harbors: Selector('a[class^="internalNavLink_"][href^="/harbors"]'),
+  winterStorageAreas: Selector('a[class^="internalNavLink_"][href^="/winter-storage-areas"]'),
   applications: Selector('a[class^="internalNavLink_"][href^="/applications"]'),
   customers: Selector('a[class^="internalNavLink_"][href^="/customers"]'),
   pricing: Selector('a[class^="internalNavLink_"][href^="/pricing"]'),
