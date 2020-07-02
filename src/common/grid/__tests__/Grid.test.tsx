13b45a6bc13d98d9aa2c83b8c6cc606f9a166611
Tatu Arvela <tatu.arvela@nitor.com>
2020-07-02 12:30:53 +0300

    Fix behavior and test coverage of common/Grid

--- src/common/grid/__tests__/Grid.test.tsx
+++ src/common/grid/__tests__/Grid.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Grid from './Grid';
+import Grid, { GridProps } from '../Grid';
 
 describe('Grid', () => {
-  const getWrapper = (props) =>
+  const getWrapper = (props?: Omit<GridProps, 'children'>) =>
     shallow(
       <Grid {...props}>
         <div>one</div>
