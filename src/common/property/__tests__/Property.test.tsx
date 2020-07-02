877cf58d7e5b542e5c56b0337a8988802d0f95c2
Tatu Arvela <tatu.arvela@nitor.com>
2020-07-02 15:09:39 +0300

    Fix test coverage of common/Property

--- src/common/property/__tests__/Property.test.tsx
+++ src/common/property/__tests__/Property.test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Property, { PropertyProps } from '../Property';
-import Text from '../../text/Text';
 import { IconFence } from '../../icons';
 
 describe('Property', () => {
@@ -14,10 +13,28 @@ describe('Property', () => {
     expect(wrapper.html()).toMatchSnapshot();
   });
 
+  it('renders normally without icon', () => {
+    const wrapper = getWrapper({ label: 'Test' });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it('renders icon with secondary color if active is false', () => {
+    const wrapper = getWrapper({ icon: IconFence, active: false, label: 'Test' });
+
+    expect(wrapper.find('IconWrapper').first().prop('color')).toBe('secondary');
+  });
+
+  it('renders text without icon with secondary color if active is false', () => {
+    const wrapper = getWrapper({ active: false, label: 'Test' });
+
+    expect(wrapper.find('Text').first().prop('color')).toBe('secondary');
+  });
+
   it('renders Text component with strong value for as prop when iconName is not supplied', () => {
     const counter = 9;
     const wrapper = getWrapper({ counter, label: 'Test' });
 
-    expect(wrapper.find(Text).first().prop('as')).toBe('strong');
+    expect(wrapper.find('Text').first().prop('as')).toBe('strong');
   });
 });
