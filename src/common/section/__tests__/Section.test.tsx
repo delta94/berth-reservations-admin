723014e3d3e1c1bf648e6d91db94ff76e9a9f5a7
Tatu Arvela <tatu.arvela@nitor.com>
2020-07-09 13:53:47 +0300

    Update test for common/Section

--- src/common/section/__tests__/Section.test.tsx
+++ src/common/section/__tests__/Section.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Section from './Section';
+import Section from '../Section';
 
 describe('Section', () => {
   it('renders normally', () => {
     const getWrapper = () => shallow(<Section>Content</Section>);
     const wrapper = getWrapper();
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 });
