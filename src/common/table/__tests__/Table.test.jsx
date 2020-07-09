6b1311a1a0620f047fe25a15f0811c358e1c08fa
Tatu Arvela <tatu.arvela@nitor.com>
2020-07-09 13:54:11 +0300

    Move test for common/Table

--- src/common/table/__tests__/Table.test.jsx
+++ src/common/table/__tests__/Table.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Table from './Table';
+import Table from '../Table';
 
 const data = [
   { a: 'foobar', b: 'foobar', c: 'foobar' },
@@ -37,6 +37,6 @@ describe('Table', () => {
   it('renders normally', () => {
     const wrapper = getWrapper();
 
-    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.render()).toMatchSnapshot();
   });
 });
