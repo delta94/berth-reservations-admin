2391f5c835d0f6575995b6252d6d3509a7875b67
Tatu Arvela <tatu.arvela@nitor.com>
2020-08-07 10:04:39 +0300

    Improve applicationList mock data

--- src/features/applicationList/__fixtures__/mockData.ts
+++ src/features/applicationList/__fixtures__/mockData.ts
@@ -1,12 +1,16 @@
-import { BERTH_APPLICATIONS } from '../__generated__/BERTH_APPLICATIONS';
+import {
+  BERTH_APPLICATIONS,
+  BERTH_APPLICATIONS_berthApplications_edges as BerthApplicationNodeEdge,
+} from '../__generated__/BERTH_APPLICATIONS';
 import { getBerthApplicationData } from '../utils';
 import { ApplicationStatus } from '../../../@types/__generated__/globalTypes';
 
 export const mockData: BERTH_APPLICATIONS = {
   berthApplications: {
     __typename: 'BerthApplicationNodeConnection',
-    count: 2,
+    count: 3,
     edges: [
+      // Full structure
       {
         __typename: 'BerthApplicationNodeEdge',
         node: {
@@ -15,11 +19,11 @@ export const mockData: BERTH_APPLICATIONS = {
           berthSwitch: {
             __typename: 'BerthSwitchType',
             berthNumber: '1',
-            harbor: '123',
+            harbor: 'MOCK-HARBOR-0',
             harborName: 'Testisatama',
-            id: '0',
-            pier: 'Testilaituri',
-            reason: { __typename: 'BerthSwitchReasonType', title: 'Syy' },
+            id: 'MOCK-BERTH-SWITCH-0',
+            pier: 'A',
+            reason: { __typename: 'BerthSwitchReasonType', title: 'Reason' },
           },
           boatLength: 6,
           boatModel: 'Marine',
@@ -30,39 +34,48 @@ export const mockData: BERTH_APPLICATIONS = {
           boatWeight: 5,
           boatWidth: 0,
           createdAt: '2020-07-16',
-          customer: { __typename: 'ProfileNode', id: '1' },
+          customer: { __typename: 'ProfileNode', id: 'MOCK-PROFILE' },
           harborChoices: [{ __typename: 'HarborChoiceType', harbor: '1', harborName: '1', priority: 0 }],
-          id: '0',
+          id: 'MOCK-APPLICATION-0',
           lease: {
             __typename: 'BerthLeaseNode',
             berth: {
               __typename: 'BerthNode',
-              number: 0,
+              number: 7,
               pier: {
                 __typename: 'PierNode',
                 properties: {
                   __typename: 'PierProperties',
                   harbor: {
                     __typename: 'HarborNode',
-                    id: '0',
-                    properties: { __typename: 'HarborProperties', name: 'Harbor' },
+                    id: 'MOCK-HARBOR-0',
+                    properties: { __typename: 'HarborProperties', name: 'Testisatama' },
                   },
-                  identifier: 'Testilaituri',
+                  identifier: 'G',
                 },
               },
             },
-            id: '0',
+            id: 'MOCK-LEASE-0',
           },
           municipality: 'Helsinki',
           status: ApplicationStatus.PENDING,
         },
       },
+      // Most structure and nulled fields
       {
         __typename: 'BerthApplicationNodeEdge',
         node: {
           __typename: 'BerthApplicationNode',
           accessibilityRequired: false,
-          berthSwitch: null,
+          berthSwitch: {
+            __typename: 'BerthSwitchType',
+            berthNumber: '2',
+            harbor: 'MOCK-HARBOR-0',
+            harborName: 'Testisatama',
+            id: 'MOCK-BERTH-SWITCH-1',
+            pier: 'A',
+            reason: null,
+          },
           boatLength: 6,
           boatModel: 'Marine',
           boatName: 'Cama la Yano II',
@@ -72,9 +85,57 @@ export const mockData: BERTH_APPLICATIONS = {
           boatWeight: 5,
           boatWidth: 0,
           createdAt: '2020-07-16',
-          customer: { __typename: 'ProfileNode', id: '1' },
-          harborChoices: [{ __typename: 'HarborChoiceType', harbor: '1', harborName: '1', priority: 0 }],
-          id: '1',
+          customer: { __typename: 'ProfileNode', id: 'MOCK-PROFILE' },
+          harborChoices: null,
+          id: 'MOCK-APPLICATION-0',
+          lease: {
+            __typename: 'BerthLeaseNode',
+            id: 'MOCK-LEASE-1',
+            berth: {
+              __typename: 'BerthNode',
+              number: 7,
+              pier: {
+                __typename: 'PierNode',
+                properties: {
+                  __typename: 'PierProperties',
+                  identifier: 'Pier Brosnan',
+                  harbor: {
+                    __typename: 'HarborNode',
+                    id: 'MOCK-HARBOR-1',
+                    properties: {
+                      __typename: 'HarborProperties',
+                      name: null,
+                    },
+                  },
+                },
+              },
+            },
+          },
+          municipality: 'Helsinki',
+          status: ApplicationStatus.PENDING,
+        },
+      },
+      // Minimum fields
+      {
+        __typename: 'BerthApplicationNodeEdge',
+        node: {
+          __typename: 'BerthApplicationNode',
+          accessibilityRequired: false,
+          berthSwitch: null,
+          boatLength: 6,
+          boatModel: 'Marine',
+          boatName: 'Cama la Yano 3-D',
+          boatDraught: 5,
+          boatRegistrationNumber: 'C 12345',
+          boatType: '1',
+          boatWeight: 5,
+          boatWidth: 0,
+          createdAt: '2020-07-16',
+          customer: { __typename: 'ProfileNode', id: 'MOCK-PROFILE-0' },
+          harborChoices: [
+            { __typename: 'HarborChoiceType', harbor: 'MOCK-HARBOR-0', harborName: 'Testisatama', priority: 0 },
+          ],
+          id: 'MOCK-APPLICATION-2',
           lease: null,
           municipality: 'Helsinki',
           status: ApplicationStatus.PENDING,
--- src/features/applicationList/__tests__/utils.test.ts
+++ src/features/applicationList/__tests__/utils.test.ts
@@ -5,7 +5,6 @@ import {
   BERTH_APPLICATIONS_berthApplications as BerthApplicationNodeConnection,
   BERTH_APPLICATIONS_berthApplications_edges as BerthApplicationNodeEdge,
 } from '../__generated__/BERTH_APPLICATIONS';
-import { ApplicationStatus } from '../../../@types/__generated__/globalTypes';
 
 const mockDataApplications = (mockData.berthApplications as BerthApplicationNodeConnection).edges;
 
@@ -22,64 +21,6 @@ describe('utils', () => {
   describe('getBerthApplicationData', () => {
     it('should return berth application data', () => {
       expect(getBerthApplicationData(mockData)).toMatchSnapshot();
-    });
-
-    it('should use defaults for nullable fields', () => {
-      const testEdge: BerthApplicationNodeEdge = {
-        __typename: 'BerthApplicationNodeEdge',
-        node: {
-          __typename: 'BerthApplicationNode',
-          accessibilityRequired: false,
-          berthSwitch: {
-            __typename: 'BerthSwitchType',
-            berthNumber: '1',
-            harbor: '1',
-            harborName: 'Harbor',
-            id: '1',
-            pier: 'Pier',
-            reason: null,
-          },
-          boatLength: 6,
-          boatModel: 'Marine',
-          boatName: 'Cama la Yano II',
-          boatDraught: 5,
-          boatRegistrationNumber: 'B 12345',
-          boatType: '1',
-          boatWeight: 5,
-          boatWidth: 0,
-          createdAt: '2020-07-16',
-          customer: { __typename: 'ProfileNode', id: '1' },
-          harborChoices: null,
-          id: '1',
-          lease: {
-            __typename: 'BerthLeaseNode',
-            id: '1',
-            berth: {
-              __typename: 'BerthNode',
-              number: 7,
-              pier: {
-                __typename: 'PierNode',
-                properties: {
-                  __typename: 'PierProperties',
-                  identifier: 'Pier Brosnan',
-                  harbor: {
-                    __typename: 'HarborNode',
-                    id: '1',
-                    properties: {
-                      __typename: 'HarborProperties',
-                      name: null,
-                    },
-                  },
-                },
-              },
-            },
-          },
-          municipality: 'Helsinki',
-          status: ApplicationStatus.PENDING,
-        },
-      };
-      const testData: BERTH_APPLICATIONS = buildTestData([testEdge, ...mockDataApplications]);
-      expect(getBerthApplicationData(testData)).toEqual(expect.not.arrayContaining([null]));
     });
 
     it('should return empty array when there are no berth applications', () => {
