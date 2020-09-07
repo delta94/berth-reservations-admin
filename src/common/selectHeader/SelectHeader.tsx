c1c15bfeea4fbfd49f7111202bf27626c4695062
Tatu Arvela <tatu.arvela@nitor.com>
2020-09-07 13:29:19 +0300

    Add title capability to SelectHeader

--- src/common/selectHeader/SelectHeader.tsx
+++ src/common/selectHeader/SelectHeader.tsx
@@ -4,34 +4,37 @@ import classNames from 'classnames';
 import styles from './selectHeader.module.scss';
 
 interface SelectHeaderProps<T> {
+  allLabel: string;
   className?: string;
+  editLabel: string;
   items: T[];
   selectedItem?: T | null;
-  allLabel: string;
-  editLabel: string;
+  title?: string;
   formatter(item: T): string;
   equals(a: T, b: T): boolean;
-  renderer?(item: T): React.ReactNode;
-  onSelect(item: T | null): void;
   onEdit?(item: T): void;
+  onSelect(item: T | null): void;
+  renderer?(item: T): React.ReactNode;
 }
 
 const SelectHeader = <T extends {}>({
+  allLabel,
   className,
+  editLabel,
   items,
   selectedItem,
-  allLabel,
-  editLabel,
+  title,
   formatter,
   equals,
-  renderer,
-  onSelect,
   onEdit,
+  onSelect,
+  renderer,
 }: SelectHeaderProps<T>) => {
   return (
     <div className={classNames(styles.container, className)}>
       <div className={styles.selectContainer}>
         <div>
+          {title && <span className={styles.title}>{title}</span>}
           <button
             onClick={() => onSelect(null)}
             className={classNames(styles.button, {
